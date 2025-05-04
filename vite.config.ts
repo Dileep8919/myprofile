a068d069fe6c1a2f42a995b7ac1e330b781a7e49
Dileep <dileepg@italentdigital.com>
2025-05-04 12:50:24 +0530

    fix vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/myprofile/', // 👈 this is important!
+  base: '/', // 👈 this is important!
 })
